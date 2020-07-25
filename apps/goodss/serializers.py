52c9ca0b68c5a7debef1df8097d8684b0863f058
huanshenyi <txy1226052@gmail.com>
2020-07-25 19:59:03 +0900

    goodssのserizlizersのuser処理を修正-自動追加可能にする

--- apps/goodss/serializers.py
+++ apps/goodss/serializers.py
@@ -5,18 +5,36 @@ from apps.user.serializers import UserSerializer
 
 
 class GoodsSerializer(serializers.ModelSerializer):
+    created_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+    owner = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        help_text="ログインしてるユーザー"
+    )
+
     class Meta:
         model = Goods
         fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    created_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+    owner = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        help_text="ログインしてるユーザー"
+    )
+
     class Meta:
         model = Category
         fields = "__all__"
 
 
 class TagSerializer(serializers.ModelSerializer):
+    created_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+    owner = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        help_text="ログインしてるユーザー"
+    )
+
     class Meta:
         model = Tag
         fields = "__all__"
